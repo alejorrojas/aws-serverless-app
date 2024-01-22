da17c76e67ba506e876c413e7ad4fa8505b98294
alejorrojas <alejoivanrojas@gmail.com>
2024-01-21 22:15:56 -0300

    new confing

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "sa-east-1_0zUKELlgw", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "30vq1pb6dsn12i9dcie2ejuv09", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "sa-east-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
